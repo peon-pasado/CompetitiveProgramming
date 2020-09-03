de8716d5477c7387c88adaf8697153ccd69a7223
Miguel Alessandro Mini H <45865868+peon-pasado@users.noreply.github.com>
2020-09-03 03:21:51 -0500

    BIT, permutation

--- infoarena/permsort2.cpp
+++ infoarena/permsort2.cpp
@@ -1,21 +1,31 @@
-//#pragma comment(linker, "/stack:200000000")
-//#pragma GCC optimize("Ofast")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+/**
+ * @name Miguel Mini
+ * @tag BIT, permutation
+ * @idea
+ *      - we can see revert prefix x + revert prefix (x-1)
+ *      as cyclic shift of the first x elements.
+ * 
+ *      - we can see that a cyclic shift is the same as 
+ *      removing the first element and putting it in an 
+ *      arbitrary position
+ *      
+ *      - we can take the first item and greedily put it 
+ *      at the bottom of the list considering how many 
+ *      larger numbers have not come up.
+**/
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1e6 + 10;
-int a[maxn];
+
+const int maxn = 1e5 + 10;
 int ft[maxn];
-int pos[maxn];
-int n;
- 
-void upd(int pos, int v) {
-	while (pos <= n) {
-		ft[pos] += v;
+
+void upd(int pos) {
+	while (pos < maxn) {
+		ft[pos]++;
 		pos += pos&-pos;
 	}
 }
- 
+
 int qry(int pos) {
 	int ans = 0;
 	while (pos > 0) {
@@ -24,25 +34,20 @@ int qry(int pos) {
 	}
 	return ans;
 }
- 
+
 int main() {
-	freopen("permsort.in", "r", stdin);	
-	freopen("permsort.out", "w", stdout);
+    freopen("permsort2.in", "r", stdin);
+    freopen("permsort2.out", "w", stdout);
+	int n;
 	scanf("%d", &n);
+	printf("%d\n", 2*n);
 	for (int i = 1; i <= n; ++i) {
-		scanf("%d", a+i);
-		pos[a[i]] = i;
-		upd(a[i], 1);
+		int x;
+		scanf("%d", &x);
+		int t = qry(x) - i + 1 + n;
+		printf("P %d\n", t);
+		printf("P %d\n", max(1, t-1));
+		upd(x);
 	}
-	pos[0] = 0;
-	long long ans = 0;
-	for (int i = 1; i <= n; ++i) {
-		ans += qry(pos[i]) - qry(pos[i-1]);
-		if (pos[i-1] > pos[i]) {
-			ans += n-i+1;
-		}
-		upd(pos[i], -1);
-	}
-	printf("%lld\n", ans);
 	return 0;
 }
