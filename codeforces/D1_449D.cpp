c0e8ec7df94e5674d9b2b5c6548c12345a8c2162
Miguel Alessandro Mini H <45865868+peon-pasado@users.noreply.github.com>
2021-04-06 03:31:21 -0500

    dp_sos, IEP

---
+++ codeforces/D1_449D.cpp
@@ -0,0 +1,108 @@
+/**
+ * @author Miguel Mini
+ * @tag dp_sos, IEP
+ * @idea
+ *    - we can use reverse order
+ *    in dp sos and calculate
+ *    dp[s] = #supersets of s.
+ * 
+ *    - with these values we 
+ *    can make IEP to calculate
+ *    
+ *      |C1 & C2 & ... C20|
+ * 
+ *    where Ci is number of sets
+ *    with at least a zero in 
+ *    i-th bit.
+ *  
+ * @complexity O(max(Ai) x log(max(Ai)) + n)
+ */ 
+#include <bits/stdc++.h>
+#define sz(x) ((int)x.size())
+#define trav(v, x) for (auto v : x)
+#define reu(x, y, z) for (int x=y; x<z; ++x)
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define eb emplace_back
+using namespace std;
+using ll = long long;
+using ii = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<ii>;
+const int mod = 1e9 + 7;
+
+int add(int a, int b, int m=mod) {
+    return a+b < mod? a+b : a+b-mod;
+}
+
+int mul(ll a, ll b, int m=mod) {
+    return a*b%mod;
+}
+
+int ex(int a, int b, int m=mod) {
+    int r=1;
+    while (b > 0) {
+        if (b&1) r = mul(r, a);
+        a = mul(a, a);
+        b >>= 1;
+    }
+    return r;
+}
+
+const int inf = INT_MAX / 2.2;
+
+class Solver {
+public:
+    void solveOne(istream& in, ostream& out) {
+      int n;
+      in >> n;
+      vi a(n);
+      rep(i, n) in >> a[i];
+      vi dp(1<<20);
+      rep(i, n) dp[a[i]]++;
+      rep(k, 20) {
+        rep(i, (1<<20)) {
+          if (i & (1<<k)) {
+            dp[i ^ (1<<k)] = add(dp[i ^ (1<<k)], dp[i]);
+          }
+        } 
+      }
+      vi pot(n+1);
+      pot[0] = 1;
+      rep(i, n) pot[i + 1] = add(pot[i], pot[i]);
+      int ans = 0;
+      rep(i, (1<<20)) {
+        if (__builtin_popcount(i)&1) ans = add(ans, mod - (pot[dp[i]] - 1));
+        else ans = add(ans, pot[dp[i]] - 1);
+      }
+      out << ans << '\n';
+    }
+    void solve(istream& in, ostream& out) {
+        out.precision(10);
+        out << fixed;
+        int testNumber = 1;
+        //in >> testNumber;
+        rep(tc, testNumber) {
+            //out << "Case #" << tc+1 << ": ";
+            solveOne(in, out);
+        }
+    }
+};
+
+int main() {
+    Solver solver;
+    string file = "";
+    if (!file.empty()) {
+      ifstream in(file + ".in");
+      ofstream out(file + ".out");
+      in.tie(nullptr);
+      in.exceptions(in.failbit);
+      solver.solve(in, out);
+    } else {
+      ios_base::sync_with_stdio(false);
+      cin.tie(nullptr);
+      solver.solve(cin, cout);
+    }
+    return 0;
+}
