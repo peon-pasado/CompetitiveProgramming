d318edc9976f86365c921ce57bc5a90120e3719f
Miguel Alessandro Mini H <45865868+peon-pasado@users.noreply.github.com>
2021-04-26 02:33:03 -0500

    grundy_number, constructive

---
+++ uva/11892.cpp
@@ -0,0 +1,42 @@
+/**
+ * @author Miguel Mini
+ * @tag grundy_number, constructive
+ * @idea
+ *      - if all elements are
+ *      1, n odd is win to first
+ *      player
+ * 
+ *      - strategy:
+ * 
+ *          - suppose k >= 1 elements
+ *          > 1:
+ * 
+ *              - first k-1 take a[i]-1
+ * 
+ *              - with last > 1, select a[i]
+ *              or a[i]-1 to force win. 
+ * 
+ * @complexity O(t n)
+ */ 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int maxn = 20010;
+int a[maxn];
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+        int r = 0;
+        for (int i = 0; i < n; ++i) {
+            scanf("%d", a+i);
+            r += a[i] == 1;
+        }
+        puts((r < n || r % 2) ? "poopi" : "piloop");
+    }
+    return 0;
+}
