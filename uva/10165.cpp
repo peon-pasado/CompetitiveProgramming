a0fadb553c401a71944a6a9d228d013a0d8cc3ff
Miguel Alessandro Mini H <45865868+peon-pasado@users.noreply.github.com>
2021-04-26 05:26:42 -0500

    nim

---
+++ uva/10165.cpp
@@ -0,0 +1,23 @@
+/**
+ * @author Miguel Mini
+ * @tag nim
+ * @idea 
+ *      - simple nim.
+ */ 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    while (cin >> n, n) {
+        int r = 0;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            r ^= x;
+        }
+        cout << (r ? "Yes" : "No") << '\n';
+    }
+    return 0;
+}
