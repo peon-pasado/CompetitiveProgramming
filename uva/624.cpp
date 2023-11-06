0f1ed87bf5ef1ce1fc45cf8ad90584e3cba0a1ef
Miguel Alessandro Mini H <45865868+peon-pasado@users.noreply.github.com>
2023-11-05 19:44:32 -0500

    Update 624.cpp

--- uva/624.cpp
+++ uva/624.cpp
@@ -1,39 +1,38 @@
-#include <iostream>
-#include <cstdio>
-#include <vector>
-#define f(x, y, z) for(int x = y; x < z; ++x)
+#include <bits/stdc++.h>
+using namespace std;
 
-using namespace::std;
+vector<int> solution;
+int disp;
+const int maxn = 22;
+int a[maxn];
+int N, n;
 
-int n, m, suma_r;
-vector<int> r, v, a;
-
-
-void backtracking(vector<int>& solve, vector<int>& set, long long suma, int indice){
-
-    if(suma > n) return;
-    if(suma > suma_r || suma == suma_r && solve.size() >= r.size()) r = solve, suma_r = suma;
-    if(indice == m) return;
-
-    solve.push_back(indice);
-    backtracking(solve, set, suma + set[indice] , indice + 1);
-    solve.pop_back();
-    backtracking(solve, set, suma               , indice + 1);
+void backtracking(int pos, int N, vector<int>& v, vector<int>& solution, int& disp) {
+	if (pos == n) {
+		if (disp > N) {
+			disp = N;
+			solution = v;
+		}
+		return;
+	}
+	
+	backtracking(pos+1, N, v, solution, disp);
+	if (a[pos] <= N) {
+		v.push_back(a[pos]);
+		backtracking(pos+1, N-a[pos], v, solution, disp);
+		v.pop_back();
+	}
 }
 
-
-int main(){
-
-    while(scanf("%d %d", &n, &m) != EOF){
-
-        r.resize(0); a.resize(0); suma_r = 0; v.resize(m);
-        f(i, 0, m) scanf("%d", &v[i]);
-    
-        backtracking(a, v, 0, 0);       
-        
-        f(i, 0, int(r.size())) printf("%d ", v[r[i]]);
-        printf("sum:%d\n", suma_r);
-    }
-
-    return 0;
-}   
+int main() {
+	while (cin >> N >> n) {
+		for (int i = 0; i < n; ++i) cin >> a[i];
+		solution.clear();
+		disp = N;
+		vector<int> v;
+		backtracking(0, N, v, solution, disp);
+		for (int e : solution) cout << e << " ";
+		cout << "sum:" << N - disp << endl;
+	}
+	return 0;
+}
