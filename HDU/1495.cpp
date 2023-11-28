d3511276e2e468e0f2933a04f14fda7580ecd4cc
Miguel Alessandro Mini H <45865868+peon-pasado@users.noreply.github.com>
2023-11-27 22:23:35 -0500

    hdu

--- HDU/1495.cpp
+++ HDU/1495.cpp
@@ -1,31 +1,63 @@
+/**
+ * @idea:
+ * 
+ *  - given the constraints, dont exist solution
+ *  if s(mod 2) = 1 o s/2 (need value) dont divide
+ *  gcd(n, m). 
+ * 
+ *  - suppose that we want reach xn + ym, we can
+ *  make (x, y) = (1, 0) and (0, 1). 
+ * 
+ *  - if n > m: (1, -1) ... (1, -2) ... n - qm = (n%m)
+ *  
+ *  - m % (n % m) ... 
+ * 
+ *  - until gcd(n, m). 
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 const int N = 101;
-bool vis[N][N][N];
 int d[N][N];
-int s[3];
+int s, n, m;
 void dfs(int x[3], int L) {
     d[x[1]][x[2]] = L;
-    if (x[1] == s[0]/2 && x[2] == s[0]/2) return;
-
+    for (int i=0; i<2; ++i)
+        for (int j=i+1; j<3; ++j)
+            if (x[i] == s/2 && x[j] == s/2) return; 
+    int y[3] = {s - x[0], n - x[1], m - x[2]};
     for (int i=0; i<3; ++i)
         for (int j=0; j<3; ++j) if (i != j) {
-            int m = min(x[i], x[j]);
+            int m = min(x[i], y[j]);
             x[i] -= m;
             x[j] += m;
+            if (d[x[1]][x[2]] > L + 1) {
+                dfs(x, L+1);
+            }
+            x[j] -= m;
+            x[i] += m;
         }
 }
 
 int main() {
     while (cin>>s>>n>>m, s) {
-        if (s % 2 == 1 || s/2 % __gcd(n, n) != 0) {
+        if (s % 2 == 1 || (s/2) % __gcd(n, m) != 0) {
             cout << "NO" << '\n';
+            continue;
         } 
-        for (int i=0; i<=n; ++i)
-            for (int j=0; j<= m; ++j)
+        for (int i=0; i<=s; ++i)
+            for (int j=0; j<=s; ++j)
                 d[i][j] = 1e9;
         
+        int x[3] = {s, 0, 0};
+        dfs(x, 0);
+
+        int ans = 1e9;
+        ans = min(ans, d[s/2][s/2]);
+        ans = min(ans, d[0][s/2]);
+        ans = min(ans, d[s/2][0]);
+        cout << ans << '\n';
     }
     return 0;
 }
---
+++ poj/3087.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int t;
+    cin>>t;
+    for (int tc=1; tc<=t; ++tc) {
+        cout << tc << ' ';
+        string s1, s2, s12;
+        cin>>s1>>s2>>s12;
+        
+    }
+
+    return 0;
+}
