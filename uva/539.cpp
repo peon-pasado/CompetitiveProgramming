d7286d97d0062f100f99eab4e4a5ca2f3233e4da
Miguel Alessandro Mini H <45865868+peon-pasado@users.noreply.github.com>
2023-12-07 00:13:22 -0500

    uva

--- uva/539.cpp
+++ uva/539.cpp
@@ -21,39 +21,30 @@ int n, m, maximum;
 vi graph[N];
 bool visit_edge[N][N];
 
-int dfs(int source, int suma){
-    
+void dfs(int source, int suma) { 
     maximum = max(maximum, suma);
-
-    for(int i = 0; i < int(graph[source].size()); ++i){
+    for (int i = 0; i < int(graph[source].size()); ++i) {
         int q = graph[source][i];
         if(!visit_edge[source][q]){
-            visit_edge[q][source] = visit_edge[source][q] = true;
+            visit_edge[q][source] = visit_edge[source][q] = 1;
             dfs(q, suma + 1);
-            visit_edge[q][source] = visit_edge[source][q] = false;
+            visit_edge[q][source] = visit_edge[source][q] = 0;
         }
     }
 }
 
-
 int main(){
-    
     while(scanf("%d %d", &n, &m), n + m){
-        
-        for(int i = 0; i < n; ++i) graph[i].clear();
-        
         for(int i = 0; i < m; ++i){
             int a, b;
             scanf("%d %d", &a, &b);
             graph[a].push_back(b);
             graph[b].push_back(a);
         }
-
         maximum = INT_MIN;
         for(int i = 0; i < n; ++i) dfs(i, 0);
-
         printf("%d\n", maximum);
+        for(int i = 0; i < n; ++i) graph[i].clear();
     }
-
     return 0;
 }
